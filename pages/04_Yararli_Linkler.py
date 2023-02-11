71da8b8248f76226343150f6c967370500f195a8
Vusal Ismayilov <woosal@pm.me>
2023-02-11 04:40:58 +0300

    Update 04_Yararli_Linkler.py

--- pages/04_Yararli_Linkler.py
+++ pages/04_Yararli_Linkler.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown("""### Kalacak Yerler (Places to Stay Free of Charge):
+st.markdown("""<details>
+    <summary><h3>Kalacak Yerler (Places to Stay Free of Charge)</h3></summary>
 
 | Yer (Place) | İletişim (Contact) |
 |:-----------|:------------|
@@ -15,22 +16,27 @@ st.markdown("""### Kalacak Yerler (Places to Stay Free of Charge):
 | Hepsi Emlak | [https://www.hepsiemlak.com/emlak-yasam/genel/dostluk-catisi](https://www.hepsiemlak.com/emlak-yasam/genel/dostluk-catisi) |
 | Otelz | [https://www.otelz.com/tr/gecmisolsunturkiyem](https://www.otelz.com/tr/gecmisolsunturkiyem) |
 | Barınma Alanları (Housing Areas) | [https://www.afetbilgi.com/G%C3%BCvenli%20Toplanma%20Alanlar%C4%B1](https://www.afetbilgi.com/G%C3%BCvenli%20Toplanma%20Alanlar%C4%B1) |
+</details>
 
-
-### Ücretsiz Veteriner ve Kedi Evleri Adresleri (Free Vet and Cat House Addresses)
+<details>
+    <summary><h3>Ücretsiz Veteriner ve Kedi Evleri Adresleri (Free Vet and Cat House Addresses)</h3></summary>
 
 | Yer (Place) | İletişim (Contact) |
 |:-----------|:------------|
 | Veteriner Listesi (Vet List) | [https://www.google.com/maps/d/u/0/viewer?mid=1quGS8qonhKrGMr0aqfrNvLi9GzvYE0I&ll=38.90781588955668%2C34.55183079999997&z=6](https://www.google.com/maps/d/u/0/viewer?mid=1quGS8qonhKrGMr0aqfrNvLi9GzvYE0I&ll=38.90781588955668%2C34.55183079999997&z=6) |
+</details>
 
 
-### Kan Bağışı (Blood Donation)
+<details>
+    <summary><h3>Kan Bağışı (Blood Donation)</h3></summary>
 
 | Yer (Place) | İletişim (Contact) |
 |:-----------|:------------|
 | Kanver Adresi | [https://kanver.org/KanHizmetleri/KanBagisiNoktalari](https://kanver.org/KanHizmetleri/KanBagisiNoktalari) |
+</details>
 
-### Deprem Alışverişi (Earthquake Shopping Donation)
+<details>
+    <summary><h3>Deprem Alışverişi (Earthquake Shopping Donation)</h3></summary>
 
 | Yer (Place) | İletişim (Contact) |
 |:-----------|:------------|
@@ -42,12 +48,14 @@ st.markdown("""### Kalacak Yerler (Places to Stay Free of Charge):
 | LC Waikiki | [https://www.lcwaikiki.com/tr-TR/TR/etiket/deprem-seferberligi](https://www.lcwaikiki.com/tr-TR/TR/etiket/deprem-seferberligi) |
 | Migros | [https://www.migros.com.tr/](https://www.migros.com.tr/) |
 | Carrefoursa| [https://www.carrefoursa.com/yardimlasmaseferberligi]() |
-
+</details>
 
 ### Kripto ve IBAN Para Bağışı (Crypto and IBAN Money Donation)
 
 #### A. International Accounts for Money Transfer
-##### Governmental and Semi Governmental Organizations (Not Suggested)
+
+<details>
+    <summary><h5>Governmental and Semi Governmental Organizations (Not Suggested)</h5></summary>
 
 | Organization / Charity | Bank Info | IBAN (EUR) | IBAN (USD) | Account No / Routing No | SWIFT Code | Extra Info |
 |:-----------|:------------|:------------|:------------|:------------|:------------|:------------|
@@ -55,9 +63,10 @@ st.markdown("""### Kalacak Yerler (Places to Stay Free of Charge):
 | [Disaster and Emergency Management Presidency](https://www.afad.gov.tr/depremkampanyasi2) | T.C. Ziraat Bankası | TR19 0001 0017 4555 5555 5552 06 | TR46 0001 0017 4555 5555 5552 05 || TCZBTR2A | Governmental Organization, widely critized for being slow and not organizing efforts to rescue |
 | [Disaster and Emergency Management Presidency](https://www.afad.gov.tr/depremkampanyasi2) | Türkiye İş Bankası | TR19 0006 4000 0024 2992 5862 85 | TR09 0006 4000 0024 2992 5862 71 || ISBKTRIS | Governmental Organization, widely critized for being slow and not organizing efforts to rescue |
 | [Embassy of the Republic of Turkey](https://www.instagram.com/p/CoU3aBlOM02/) | Bank of America ||| 0019 2343 0455 / ABA: 054001204 | BOFAUS3N | Governmental Organization |
-||||||||
+</details>
 
-##### Non-Governmental Organizations (Suggested)
+<details> 
+    <summary><h5>Non-Governmental Organizations (Suggested)</h5></summary>
 
 | Organization / Charity | Bank Info | IBAN (EUR) | IBAN (USD) | IBAN (GDP) | SWIFT Code | Extra Info |
 |:-----------|:------------|:------------|:------------|:------------|:------------|:------------|
@@ -65,8 +74,10 @@ st.markdown("""### Kalacak Yerler (Places to Stay Free of Charge):
 | [AKUT - Arama Kurtarma Derneği](https://www.akut.org.tr/en/about-akut) | Türkiye İş Bankası | TR12 0006 4000 0021 0806 6661 44 | TR48 0006 4000 0021 0806 6666 60 || ISBKTRIS | NGO, Search and Rescue focus |
 | [AKUT - Arama Kurtarma Derneği](https://www.akut.org.tr/en/about-akut) | QNB Finansbank | TR66 0011 1000 0000 0032 4941 26 | TR82 0011 1000 0000 0032 4941 29 || FNNBTRIS | NGO, Search and Rescue focus |
 | [Bündnis Entwicklung Hilft (BEH) & Aktion Deutschland Hilft (ADH)](https://www.spendenkonto-nothilfe.de/) | Commerzbank | DE53 200 400 600 200 400 600 ||| COBADEFF | NGO, see website for alternative payment options (German) |
+</details>
 
-#### B. International Funds or Organizations
+<details>
+<summary><h4>B. International Funds or Organizations</h4></summary>
 
 | Organization / Charity | Link | Credit Card | Paypal | Alternative Payment | Info | Extra Info |
 |:-----------|:------------|:------------|:------------|:------------|:------------|:------------|
@@ -82,27 +93,95 @@ st.markdown("""### Kalacak Yerler (Places to Stay Free of Charge):
 | French Red Cross | [Donner Croix-Rouge](https://donner.croix-rouge.fr/seisme-moyen-orient/~mon-don) | Yes | Yes | Direct Payment | NGO, part of International Red Cross and Red Crescent Movement | France |
 | Lithuanian Red Cross | [Lietuvos Raudonojo Kryžiaus](https://redcross.lt/naujienos-ir-renginiai/padekime-isgelbeti-gyvybes/) | Yes || Direct Payment, SMS | NGO, part of International Red Cross and Red Crescent Movement | Lithuania |
 | Oxfam Australia | [Donate Oxfam](https://secure.oxfam.org.au/donate/turkey-syria-earthquake) | Yes ||| NGO | Australia |
-||||||||
-||||||||
-||||||||
-||||||||
-||||||||
+| Oxfam New Zealand | [Donate Oxfam](https://www.oxfam.org.nz/appeals/disaster-response/) | Yes ||| NGO | New Zealand |
+| Turkish Philanthropy Funds | [Donate TPFUND](https://donate.tpfund.org/give/161251/#!/donation/checkout) | Yes | Yes || NGO | USA |
+| Bridge to Turkiye | [Bridge to Turkiye](https://bridgetoturkiye.org/btf-s-friends-of-ahbap-giving-circle/) | Yes || Direct Payment | Fundraiser to help Ahbap Platform | USA |
+| Humanity & Inclusion | [HI-US](https://www.hi-us.org/emergencies-middle-east-earthquakes-claim-lives-bring-more-instability-region) | Yes | Yes | Google Pay | Fundraiser focuses on mental health and rehabilitation | USA |
+| DITIB - Türkisch-Islamische Union der Anstalt für Religion | [DITIB](https://www.ditib.de/spende/deprem/index.html) | Yes | Yes | SEPA | NGO, connected to Turkish Directorate of Religious Affairs | Germany |
+| Turkiye Earthquake Relief Fund | [Youtube Video - Fundraiser](https://www.youtube.com/watch?v=-BvS6PZbCws) | Yes ||| Youtube Campaign, Turkish Philanthropy Funds ||
+| Islamic Relief USA | [Donate Now](https://irusa.org/middle-east/turkey/) | Yes || Direct Payment | NGO | USA |
+| Muslim Hands UK | [Donate Now](https://muslimhands.org.uk/donate/turkey-and-syria-earthquake-emergency/turkey-and-syria-earthquake-emergency) | Yes ||| NGO | UK |
+| International Development and Relief Foundation | [IDRF.ca Donate](https://idrf.ca/project/turkiye-and-syria-earthquake-relief/) | Yes ||| NGO | Canada |
+| Molham Volunteering Team | [Campaign Donate](https://molhamteam.com/en/campaigns/439) | Yes ||| NGO | Jordan |
+</details>
+
+<details>
+    <summary><h4> C. Cryptocoin Donations</h4></summary>
+
+| Organization / Charity | ERC20 Wallet | BEP20 Wallet | Avalanche Wallet | Info |
+|:-----------|:------------|:------------|:------------|:------------|
+| [Ahbap](https://twitter.com/haluklevent/status/1622926244512661504?s=20) | 0xe1935271D1993434A1a59fE08f24891Dc5F398Cd | 0xB67705398fEd380a1CE02e77095fed64f8aCe463 | 0x868D27c361682462536DfE361f2e20B3A6f4dDD8 | NGO, focuses on humanitarian aid |
+</details>
+
+<details>
+    <summary><h3>A. Bağış Yapılabilecek Banka Hesapları</h3></summary>
+
+| Organizasyon | Banka | IBAN | İnternet Sitesi Üzerinden |
+|:-----------|:------------|:------------|:------------|
+| Türkiye Kızılay Derneği | T.C. Ziraat Bankası | TR48 0001 0021 1000 0028 6852 54 | [https://www.kizilay.org.tr/Bagis/BagisYap/404/pazarcik-depremi-bagisi](https://www.kizilay.org.tr/Bagis/BagisYap/404/pazarcik-depremi-bagisi) |
+| AFAD | T.C. Ziraat Bankası | TR73 0001 0017 4555 5555 5552 04 | [https://www.afad.gov.tr/depremkampanyasi2](https://www.afad.gov.tr/depremkampanyasi2) |
+| Ahbap Derneği | Türkiye İş Bankası | TR12 0006 4000 0011 0211 3800 59 | [https://ahbap.org/bagisci-ol](https://ahbap.org/bagisci-ol) |
+| AKUT | Türkiye İş Bankası | TR14 0006 4000 0011 0800 6666 63 | [https://www.akut.org.tr/bagis-yap](https://www.akut.org.tr/bagis-yap) |
+</details>
+
+<details>
+    <summary><h4>B. Maddi Bağış Yapılabilecek Alternatif Yollar</h4></summary>
+
+| Organizasyon | Link | Bilgi |
+|:-----------|:------------|:------------|
+| İzmir Umut Hareketi | [https://umuthareketi.izmir.bel.tr/](https://umuthareketi.izmir.bel.tr/) | İzmir Büyükşehir Belediyesi |
+| Ahbap Derneği | [https://www.gofundme.com/f/xh8d5t-turkey-earthquake-fund](https://www.gofundme.com/f/xh8d5t-turkey-earthquake-fund) | STK |
+| Ahbap Derneği (Cryptocoin) | [https://twitter.com/haluklevent/status/1622926244512661504](https://twitter.com/haluklevent/status/1622926244512661504) | STK |
+| Ahbap Derneği (Trendyol) | [https://www.trendyol.com/sr?mid=405374&os=1](https://www.trendyol.com/sr?mid=405374&os=1) | STK Hediye Kartı |
+| Ahbap Derneği (Amazon) | [https://www.amazon.com.tr/dp/B0BV3SGX7N](https://www.amazon.com.tr/dp/B0BV3SGX7N) | STK Hediye Kartı |
+| N11 | [https://www.n11.com/promosyon/deprem-yardimlasma-seferberligi-1600359](https://www.n11.com/promosyon/deprem-yardimlasma-seferberligi-1600359) | Özel Şirket |
+| HepsiBurada | [https://www.hepsiburada.com/deprem-seferberligi](https://www.hepsiburada.com/deprem-seferberligi) | Özel Şirket |
+| TrendYol | [https://www.trendyol.com/sr?cid=619322&pi=2](https://www.trendyol.com/sr?cid=619322&pi=2) | Özel Şirket |
+| HepsiBurada Hediye Kartları | [https://www.hepsiburada.com/kampanyalar/deprem-yardim-kartlari](https://www.hepsiburada.com/kampanyalar/deprem-yardim-kartlari) | Özel Şirket + NGO İşbirliği |
+| Gıda Bağışı (Esnaf, Restoran, Bakkal vb.) | [https://docs.google.com/forms/d/e/1FAIpQLSdi_oW8eTwkHHRjDBxwX36t8hxcrA8Jjx8fag-xQzXhd9iwfg/viewform](https://docs.google.com/forms/d/e/1FAIpQLSdi_oW8eTwkHHRjDBxwX36t8hxcrA8Jjx8fag-xQzXhd9iwfg/viewform) | Gönüllü Listesi |
+</details>
+
+
+<details>
+    <summary><h4>C. İlgili Bağlantılar</h4></summary>
+
+| Organizasyon | Amaç | Anahtar Kelime |
+| --- | --- | --- |
+| [Deprem.io](https://deprem.io/) | Deprem ile ilgili çok amaçlı bilgi kaynağı | Harita, Yardım Talebi, Yardım Sağlama, Yardım İhtiyaç Listesi, Yardım Edenler Listesi, Konaklama Adresleri |
+| [Afet Bilgi](https://www.afetbilgi.com/) | Deprem ile ilgili çok amaçlı rehber liste | Yardım Sağlama, Toplanma Yerleri, Bağış Noktası, Veterinerler, Konaklama Adresleri, Beslenme Adresleri |
+| [Deprem Yardım](https://depremyardim.com/) | Deprem yardım talepleri | Yardım Talebi, Yardım İhtiyaç Listesi |
+| [Kurtar.me](https://kurtar.me/) | Acil ihtiyaç talepleri | Harita, Yardım Talebi |
+| [Deprem Bilgi (Google Drive)](https://drive.google.com/drive/folders/14eQPi5QG9YD5IG-SFmHoyglDmC61IHDI) | Deprem ile ilgili çok amaçlı bilgi kaynağı | Konaklama Adresleri, Beslenme Adresleri, Yardım Talepleri, Yakıt Adresleri, Kurtulanların Listesi, Hastanedekilerin Listesi, Vefat Edenlerin Listesi |
+| [İstanbul Toplama Merkezleri](https://docs.google.com/spreadsheets/d/1B0epkFl-4dF4FINjTSONWbHivOu702RBIWYnOM1gWkg/htmlview?usp=sharing) | İstanbul için eşya toplayan ve eşyaları sınıflara ayıran, kolileyen vs. işgücü ihtiyacı | Yardım Sağlama, Bağış Noktası |
+| [İzmir Toplama Merkezleri](https://docs.google.com/spreadsheets/d/e/2PACX-1vTfHhRUagvHVBsRmi94JNLpQnzPrGJZlJ_9ehpbmCBqR2TJKsXjkAY9NP0AmBa5nuiIFxkFTwMWtGCV/pubhtml) | İzmir için eşya toplayan ve eşyaları sınıflara ayıran, kolileyen vs. işgücü ihtiyacı | Yardım Sağlama, Bağış Noktası |
+| [Ankara Toplama Merkezleri](https://docs.google.com/spreadsheets/d/e/2PACX-1vSha8soQR5PWU5FjxLmmcMsHI4p6xUtd5V-oSQG_sd9dhP43rLxkNOceRxi59P1ClmNmTxe1mBtgPos/pubhtml) | Ankara için eşya toplayan ve eşyaları sınıflara ayıran, kolileyen vs. işgücü ihtiyacı | Yardım Sağlama, Bağış Noktası |
+| [Deprem Yardım Link Arşivi](https://linkstap.com/depremyardim) | Çok amaçlı link arşivi | - |
+| [Deprem Haritası (Basarsoft)](https://deprem.basarsoft.com.tr/V2) | Afet haritası | Harita, Yardım Talebi, Yardım Sağlama |
+</details>
+
+<details>
+    <summary><h3>SOCAR Petrol İstasyonları</h3></summary>
+
+| İl | İlçe | Kısa Ad | Tesis Adresi | TELEFON | KONUM |
+|----|------|---------|--------------|---------|-------|
+| ADIYAMAN | MERKEZ | AND DERECİ | Karapınar Mahallesi Karapınar Caddesi No:253/A ( Ada:3700 , Pafta:- , Parsel:1 ) | 0532 585 20 32 | [Harita](https://goo.gl/maps/MaqBjkY2jAkdt5nT7) |
+| ADIYAMAN | KAHTA | ERDOĞANLAR GÜBRE | Yavuz Selim Mahallesi M.kemal Caddesi No:15 ( Ada: - , Pafta:19 k II-b , Parsel:576 ) | 0530 348 18 99 | [Harita](https://goo.gl/maps/Jy2Kf5EwjPaBHjwg8) |
+| AKSARAY | MERKEZ | ALTINSOY RAMSÖY | No:118 Taşpınar Kasabası Yavuzselim Mahallesi Sümrü Küme Evleri ( Ada:386 , Pafta:L31-B-25-C , Parsel:1 ) | 0537 543 71 11 | [Harita](https://goo.gl/maps/bsnhufgicsPU3EATA) |
+| GAZİANTEP | ŞEHİTKAMİL | ÖZEMİN GIDA 2 | Aktoprak Mahallesi Kahramanmaraş Caddesi No:88 ( Ada:432 , Pafta:N38C06A2C , Parsel:4 ) | 0542 794 58 96 | [Harita](https://goo.gl/maps/zWMvwzQLy5tjDkvv9) |
+| HATAY | DÖRTYOL | HİDAYET PETROL | Özerli, Hatay İskenderun Yolu | 0532 603 54 19 | [Harita](https://goo.gl/maps/u9HceAXiG5zepTq39) |
+| KAHRAMANMARAŞ | TÜRKOĞLU | NEMPET YAKIT | İstasyon Mahallesi Adana Asfaltı Caddesi No:39/A ( Ada:335 , Pafta:- , Parsel:12 ) | 0536 034 04 46 | [Harita](https://goo.gl/maps/aeM8JWKCSXJ2Uc739) |
+| KAHRAMANMARAŞ | ONİKİŞUBAT | SAYLAK SAĞLIK 2 | Hacımustafa Mahallesi Gazi Mustafa Kemal Bulvarı No:192 A ( Ada:3621 , Pafta:- , Parsel:20 ) | 0531 650 28 31 | [Harita](https://goo.gl/maps/gv8ETQBUddaJ6aj69) |
+| KAHRAMANMARAŞ | ONİKİŞUBAT | OKM ENERJİ 1 | Gayberli Mahallesi Batı Çevre Yolu Bulvarı No:39 A ( Ada:4466 , Pafta:- , Parsel:13 ) | 0507 353 73 08 | [Harita](https://goo.gl/maps/EuYwDq31tsqCDTTK8) |
+| KAHRAMANMARAŞ | DULKADİROĞLU | OKM ENERJİ 2 | Mehmet Akif Mahallesi Recep Tayyip Erdoğan Bulvarı No:104/A ( Ada:4508 , Pafta: - , Parsel:9 ) | 0507 353 73 08 | [Harita](https://goo.gl/maps/PY1XsLRWC61ffAR4A) |
+| MERSİN | MUT | PETROLİM PETROL 1 | Karşıyaka Mahallesi Mustafa Kemal Bulvarı No:59 ( Ada:513 , Pafta:- , Parsel:1 ) | 0541 841 33 51 | [Harita](https://goo.gl/maps/2tRpGw5whhzsgm6t6) |
+| MERSİN | MUT | PETROLİM PETROL 2 | Kurtsuyu Mahallesi Kurtsuyu Sokağı No:48A ( Ada:111 , Pafta:- , Parsel:44 ) | 0541 841 33 51 | [Harita](https://goo.gl/maps/u7sy1n1aDZa8DH4QA) |
+| MERSİN | TARSUS | ÖZMİRPET AKARYAKIT | Yenice Mah. Mahallesi Atatürk_3 Bulvarı Bulvarı No:5/A TARSUS/MERSİN | 0542 325 12 63 | [Harita](https://goo.gl/maps/tCYoemQGZxx4Zu517) |
+| MERSİN | AKDENİZ | AYÇİÇEK PETROL | Şevket Sümer Mahallesi Okan Merzeci Bulvarı No:15/A | 0532 675 41 33 | [Harita](https://goo.gl/maps/w12JiQ52XuFK2uGx5) |
+| ŞANLIURFA | EYYÜBİYE | ATMACA AUTO | Batıkent Mahallesi Evren 1 Caddesi No:35 ( Ada:217 , Pafta:- , Parsel:1 ) | 0536 034 04 46 | [Harita](https://goo.gl/maps/xGZS2Cxzsr396DPH6) |
+| ŞANLIURFA | EYYÜBİYE | BAKIR PETROL | Akçamescit Mahallesi Akçakale Caddesi No:630/1 ( Ada:150 , Pafta: - , Parsel:1 ) | 0532 355 82 90 | [Harita](https://goo.gl/maps/FmcvR8DVHBaysuU58) |
+| ŞANLIURFA | BOZOVA | CANKARATAŞ PETROL | Gölbaşı Mahallesi Atatürk Baraj Yolu Üzeri ( Ada: - , Pafta: - , Parsel: - ) | 0532 433 85 68 | [Harita](https://goo.gl/maps/SJmXE54Aw55YazV59) |
+| ŞANLIURFA | BİRECİK | ÖZACARLAR PETROL | Karşıyaka Mahallesi Nizip Yolu Caddesi No:10/1 ( Ada:166 , Pafta: - , Parsel:46 ) | 0544 543 34 63 | [Harita](https://goo.gl/maps/2qpCL9s8fcwqb9nf9) |
+| ŞANLIURFA | HALFETİ | ACARPET AKARYAKIT | Yukarıgöklü Mahallesi Sağlık Ocağı Caddesi No:53 ( Ada: - , Pafta: - , Parsel:1700 ) | 0542 462 63 22 | [Harita](https://goo.gl/maps/CFSFJCizPTQNxFkd7) |
+| ŞANLIURFA | HİLVAN | ÇAKMAKTEKSTİL | Yeni Mahallesi ( Ada: - , Pafta: - , Parsel: - ) | 0533 243 99 31 | [Harita](https://goo.gl/maps/dr9fRHjX9cTNApyq6) |
+</details>
 """)
-
-
-st.markdown("## Kripto ve IBAN Para Bağışı (Crypto and IBAN Money Donation):")
-
-dict = {
-    "Organization / Charity":[],
-    "Bank Info":[],
-    "IBAN (EUR)":[],
-    "IBAN (USD)":[],
-    "Account No / Routing No":[],
-    "SWIFT":[],
-    "Extra Info":[]
-}
-iban_bagis_df =pd.DataFrame().from_dict({})
-
-
-
-
