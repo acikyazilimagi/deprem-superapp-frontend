ba91645627e35c662e1d2bd8cefabc99530a3409
Bahadır Araz <b123qweasdzxc@gmail.com>
2023-02-13 18:43:28 +0300

    Update 05_KVKK_Metni.py

--- pages/05_KVKK_Metni.py
+++ pages/05_KVKK_Metni.py
@@ -5,7 +5,35 @@ import pandas as pd
 import streamlit as st
 
 from helpers.http_helpers import http_helper
+url = os.getenv("API_BASE_ENDPOINT") + os.getenv("API_GET_CALL_MAP_DATA_ENDPOINT")
+response = http_helper.send_request(url, method="POST", body_params={})
 
+data = response.json()
+df = pd.DataFrame(data)
+csv_string = df.to_csv(index=False)
+actual_data = data["detail"]
+csv_strings = pd.DataFrame(actual_data)
+csv_strings = csv_strings.drop(columns=['_id'])
+csv_strings['gereksinimler'] = csv_strings['gereksinimler'].apply(lambda x: ','.join(map(str, x)))
+csv = io.StringIO(csv_strings.to_csv(index=False))
+b64 = base64.b64encode(csv.getvalue().encode()).decode()
+href = f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Yardım İhityaçları Verisini İndir</a>'
+st.markdown(href, unsafe_allow_html=True)
+
+url2 = os.getenv("API_BASE_ENDPOINT") + os.getenv("API_GET_HELPER_MAP_DATA_ENDPOINT")
+response2 = http_helper.send_request(url2, method="POST", body_params={})
+
+data2 = response2.json()
+df2 = pd.DataFrame(data2)
+csv_string2 = df2.to_csv(index=False)
+actual_data2 = data2["detail"]
+csv_strings2 = pd.DataFrame(actual_data2)
+csv_strings2 = csv_strings2.drop(columns=['_id'])
+csv_strings2['servis'] = csv_strings2['servis'].apply(lambda x: ','.join(map(str, x)))
+csv2 = io.StringIO(csv_strings2.to_csv(index=False))
+b642 = base64.b64encode(csv2.getvalue().encode()).decode()
+href2 = f'<a href="data:file/csv;base64,{b642}" download="myfilename.csv">Yardım Noktaları Verisini İndir</a>'
+st.markdown(href2, unsafe_allow_html=True)
 st.markdown("""
 ## KVKK:
 
